47a9553b7173311325c861d5faa9934ee7652cae
AdarshRai0 <adarshrai3011@gmail.com>
2023-10-04 20:32:55 +0530

    added new section

--- script.js
+++ script.js
@@ -116,3 +116,11 @@ t3.to(
   },
   "a"
 );
+
+// Add squares
+
+const squares = document.querySelector(".squares");
+for (var i = 1; i < 365; i++) {
+  const level = Math.floor(Math.random() * 3);
+  squares.insertAdjacentHTML("beforeend", `<li data-level="${level}"></li>`);
+}
